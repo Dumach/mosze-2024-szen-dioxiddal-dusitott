18a1cb360e9cd4f3f9936ec88c43c0f218362c92
Marcell Szabo <marcellszabo77@gmail.com>
2024-11-03 12:33:33 +0100

    pausemennu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    public AudioSource backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+            isPaused = true;
+
+        if(backgroundMusic != null)
+        {
+            backgroundMusic.Pause();
+        }
+
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+            isPaused = false;
+
+        if ( backgroundMusic != null)
+        {
+            backgroundMusic.UnPause();
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+}
+
