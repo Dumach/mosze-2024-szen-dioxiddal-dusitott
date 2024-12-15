035c769e8a996d2a8b03810aba7ce672b35fbbe7
Marcell Szabo <marcellszabo77@gmail.com>
2024-12-15 14:23:50 +0100

    Level Selector Panel

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private const int totalLevels = 9;
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Level1"))
+        {
+            // Az 1. szint elérhető
+            PlayerPrefs.SetInt("Level1", 1);
+            for (int i = 2; i <= totalLevels; i++)
+            {
+                // Többi szint zárolva
+                PlayerPrefs.SetInt("Level" + i, 0);
+            }
+        }
+    }
+
+    // Ellenőrizzük, hogy a szint elérhető-e
+    public bool IsLevelUnlocked(int level)
+    {
+        return PlayerPrefs.GetInt("Level" + level) == 1;
+    }
+
+    // Szint feloldása
+    public void UnlockLevel(int level)
+    {
+        PlayerPrefs.SetInt("Level" + level, 1);
+    }
+}
